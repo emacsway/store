7677467ef5d988c453d108596e8c24af9e2e3fa8
emacsway <ivzak@yandex.ru>
2018-10-21 22:30:17 +0300

    Added comments about obstacle for immutable Query

--- store.js
+++ store.js
@@ -1536,7 +1536,7 @@ function namespace(root) {
                 },
                 visit: function(owner, left, right, query) {
                     owner._promises.push(right);
-                    when(right, function(right) {
+                    when(right, function(right) {  // TODO: It's an obstacle for immutable Query.
                         query[left] = right;
                     });
                 }
@@ -1650,6 +1650,7 @@ function namespace(root) {
                         var relatedQuery = right[opName];
                         var relationName = left;
                         var relation = owner._store.getRelation(relationName);
+                        // TODO: It's an obstacle for immutable Query.
                         andClause.push(when(relation.unfoldRelatedQuery(relatedQuery), function(orClause) {
                             owner._subjects.push(orClause);
                             return {'$or': orClause};
